bc5f4d37c054c8d3d3c658f3c0882cfbd9877279
pkjy <a214161398@hotmail.com>
2018-03-18 03:01:31 +0800

    initial commit

---
+++ server/rest/admin/export.js
@@ -0,0 +1,14 @@
+/*
+* @ author sessionboy 
+* @ github https://github.com/sessionboy
+* @ website http://sinn.boyagirl.com
+* @ use 统一引入和导出前端，以及管理后台的router
+*/
+var express = require("express")
+var router = express.Router()
+
+const backendRouter = require("./routes/users")
+const backendIndex = require("./routes/index")
+router.all("*", backendRouter, backendIndex)
+
+module.exports = router
