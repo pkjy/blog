26cd7626686ccecdf060e8b3d1ad6556d166039f
pkjy <pkjy@outlook.com>
2018-09-20 00:49:00 +0800

    style(bing): add bing converstory

--- client/src/components/MainPoster.vue
+++ client/src/components/MainPoster.vue
@@ -3,8 +3,15 @@
     <NavBarMenu></NavBarMenu>
     <section class="poster-mask"></section>
     <section class="poster-conver-story">
+      <section class="content">
+        <div>
+          <h3 class="inline-block">{{todayConverstoryResponse.title}}</h3>
+          <h4 class="inline-block m-l_4 f-s_13">{{todayConverstoryResponse.attribute}}</h4>
+        </div>
+        <p class="f-s_13">{{todayConverstoryResponse.para1}}</p>
+      </section>
       <footer class="footer">
-        湘ICP备18012183号
+        {{currentImage.copyright}} | 湘ICP备18012183号
       </footer>
     </section>
   </section>
@@ -17,7 +24,12 @@ import { BING_BASE_URL } from '@/config'
 export default {
   name: 'MainPoster',
   components: { NavBarMenu },
-  computed: {},
+  computed: {
+    currentImage() {
+      if (!this.todayImageResponse.images) return {}
+      return this.todayImageResponse.images[0]
+    }
+  },
   data() {
     return {
       todayImageResponse: {},
@@ -29,21 +41,21 @@ export default {
       const formData = { format: 'js', idx: 0, n: 1 }
       const res = await apiGetImage(formData)
       this.todayImageResponse = res.data
-      this.$refs.PosterWrap.style.background = `url(${BING_BASE_URL}/${
+      this.$refs.PosterWrap.style.background = `url(${BING_BASE_URL}${
         this.todayImageResponse.images[0].url
       })`
-      console.log('res', res)
+      this.getConverstory()
+      // console.log('res', res)
     },
     async getConverstory() {
-      const formData = { d: 20180919 }
+      const formData = { d: this.currentImage.enddate }
       const res = await apiGetConverstory(formData)
       this.todayConverstoryResponse = res.data
-      console.log('res', res)
+      // console.log('res', res)
     }
   },
   mounted() {
     this.getImage()
-    this.getConverstory()
   }
 }
 </script>
@@ -68,6 +80,25 @@ export default {
   height: 100%;
   background-color: rgba(0, 0, 0, 0.2);
 }
+.poster-conver-story {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  align-items: center;
+  display: flex;
+}
+.poster-conver-story .content {
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  /* align-items: center; */
+  color: #fff;
+}
+
 .poster-conver-story .footer {
   position: absolute;
   bottom: 0;
