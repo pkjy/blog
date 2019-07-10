79a6cd1c362c3efb4156419e21184116bc67433e
pkjy <pkjy@outlook.com>
2019-07-11 01:09:04 +0800

    fix(api url): fix api url

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -8,5 +8,5 @@ export const pattMobileMosaic = new RegExp(/(\d{3})\d*(\d{4})/)
 export const pattNotes = new RegExp(/^([a-z0-9]{6})?$/)
 export const pattFloat = new RegExp(/^[1-9]\d*\.\d*|0\.\d*[1-9]\d*$/) // 正浮点数
 
-export const PROXY_BASE_URL = 'https://www.pkjy.xyz/bing'
+export const PROXY_BASE_URL = '//pkjy.xyz/bing'
 export const BING_BASE_URL = 'https://cn.bing.com'
